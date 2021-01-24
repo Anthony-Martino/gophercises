a2c61417aa0f647a4d256886abead8c6a6b0e9db
Anthony Martino <Amartino@lb.com>
2021-01-24 17:27:30 -0500

    add csv flag, read and parse csv file

--- quiz/main.go
+++ quiz/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+type problem struct {
+	q string
+	a string
+}
+
 func main() {
+	csvFilename := flag.String("csv", "problems.csv", "The name of the csv containing the problem set.")
+	flag.Parse()
+
+	csvFile, err := os.Open(*csvFilename)
+	if err != nil {
+		log.Fatalln("Error opening csv file", err)
+	}
+
+	r := csv.NewReader(csvFile)
+
+	rows, err := r.ReadAll()
+
+	if err != nil {
+		log.Fatalln("Error parsing csv file", err)
+	}
+	for i, row := range rows {
+		fmt.Println("Problem", i+1, row[0], "=")
+	}
 
 }
